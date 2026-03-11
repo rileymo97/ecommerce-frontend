51e91e9ee2279f9ac3c2bdb0ed5923cb051ba148
Riley Morrison <rileymo@uchicago.edu>
2026-03-11 18:42:47 -0500

    update store title to Riley's E-Commerce Store

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
     return (
         <div>
-            <h1>E-Commerce Store</h1>
+            <h1>Rileys E-Commerce Store</h1>
             <h2>Products</h2>
             <div>
                 {products.map(product => (
